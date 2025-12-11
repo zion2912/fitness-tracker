739de7c9c9d8dbc1644a3fa15734c49dd992ffb7
unknown <zion_nicholls@hotmail.com>
2025-12-10 21:32:46 -0400

    x

---
+++ src/config/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUe-vM78LYxTLt5QMgwN01KjNWzZ3xCLA",
+  authDomain: "fitness-tracker-16b52.firebaseapp.com",
+  projectId: "fitness-tracker-16b52",
+  storageBucket: "fitness-tracker-16b52.firebasestorage.app",
+  messagingSenderId: "1017549431286",
+  appId: "1:1017549431286:web:8044c81de25e7406da67d7",
+  measurementId: "G-MEX47ZQ68T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
